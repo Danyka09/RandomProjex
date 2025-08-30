7ecd32d77e14929179798ea4d1110e5eb4867043
Danyka09 <68117297+Danyka09@users.noreply.github.com>
2025-08-30 22:43:11 +0200

    Added time tracking. Improved, actually working dice Better leaderboard Menu is a function Actual score calculator Added clear screen Added dice total Time: 03:21:08 (yesterday was wrong, it was actually around 2h not 2h50m)

--- Planet Jumper/main.py
+++ Planet Jumper/main.py
@@ -1,7 +1,7 @@
 # This is a choice based adventure game called "Planet Jumper:Mercy of Dice".
 # It is my first ever project made without  a tutorial
 # the first bit of code was written: 27.08.2025 and this project was finished: __.__.2025
-# A total of __hours and __minutes have been put in. 02:52:52(excluding the creative part)
+# A total of __hours and __minutes have been put in. 03:21:08(excluding the creative part)
 import time
 import random
 import json
@@ -15,10 +15,17 @@ except:
 hp = 100
 healing = 30
 name = ""
+dice_total = 0
+start_time = time.time()
+
+def clear_screen():
+    print("\n" * 10)
 
 def dice_roll():
-    return random.randint(1, 6)
-roll = dice_roll()
+    global dice_total
+    roll = random.randint(1, 6)
+    dice_total = dice_total + roll
+    return roll
 
 inventory = {
     "heal": 3,
@@ -36,8 +43,6 @@ roll_damage = {
     6: +40
 }
 
-print("\n Planet Jumper: Mercy of Dice \n\n  [1] Enter the Spaceship\n  [2] View Emperors\n  [3] Chicken out ")
-
 def opt_one():
     global name
     print(f"You decided to Enter the Spaceship ")
@@ -48,8 +53,17 @@ def opt_one():
 
 def opt_two():
     global leaderboard
+    sorted_list = sorted(leaderboard, key=lambda x: x["score"], reverse=True)
     print(f"You chose to view the Emperors")
-    print (leaderboard)
+    time.sleep(0.5)
+    print ("\n  Leaderboard 🏆")
+    print("-" * 20)
+    for rank, entry in enumerate(sorted_list, 1):
+        print(f'{rank}. {entry["name"]:<10} | {entry["score"]:<5}')
+    time.sleep(1)
+    input("\nPress Enter to return to menu ")
+    clear_screen()
+    menu()
 
 def opt_three():
     print(f"You chose to chicken out")
@@ -64,21 +78,36 @@ choices = {
     "3": opt_three
 }
 
-while True:
-    menu = input("\n Choose an option: ")
-    if menu in choices:
-        choices[menu]()
-        break
-    else:
-        print("You don't have free will, choose one of the options above.")
+def menu():
+    print("\n Planet Jumper: Mercy of Dice \n\n  [1] Enter the Spaceship\n  [2] View Emperors\n  [3] Chicken out ")
+    while True:
+        menu = input("\n Choose an option: ")
+        if menu in choices:
+            choices[menu]()
+            break
+        else:
+            print("You don't have free will, choose one of the options above.")
 
-score = 200
+menu()
+
+#test rolls
+print(dice_roll())
+print(dice_roll())
+print(dice_roll())
+
+end_time = time.time()
+total_time = end_time - start_time
+score = round(hp * 10 + dice_total * 3 - total_time / 10)
 
 leaderboard.append({"name":name, "score":score})
 
 with open("leaderboard.json", "w") as file:
     json.dump(leaderboard, file, indent=4)
 
-print(leaderboard)
-print(roll)
+#Test prints
+print(dice_total)
+print(start_time)
+print(end_time)
+print(total_time)
 print(name)
+print(score)
